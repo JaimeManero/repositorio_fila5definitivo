d42679601421928e0aa2b8d7fa548d3c5c942d1e
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-23 13:38:51 +0200

    He creado la rama palindromo y hecho el ejercicio

--- src/main.java
+++ src/main.java
@@ -3,5 +3,26 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Introduce una frase (puede tener puntos, comas y espacios): ");
+        String s=sc.nextLine();
+
+        s=s.replace(" ", "");
+        s=s.replace(",", "");
+        s=s.replace(".", "");
+        int fin = s.length()-1;
+        int ini=0;
+        boolean espalin=true;
+
+        while(ini < fin){
+            if(s.charAt(ini)!=s.charAt(fin)){
+                espalin=false;
+            }
+            ini++;
+            fin--;
+        }
+        if(espalin)
+            System.out.print("\nEs palindromo.");
+        else
+            System.out.print("\nNo es palindromo.");
     }
 }
